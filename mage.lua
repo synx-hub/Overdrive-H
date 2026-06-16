c728e364b7480dad3d836596d1e03ff31edd3231
synx-hub <azuresp3@gmail.com>
2026-06-16 22:56:18 +0800

    Update mage.lua

--- mage.lua
+++ mage.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/1an5irCTnq/0"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/synx-hub/dev/refs/heads/main/obfuscated_0JuHjzfo6xInyOLlYnRuotK9KW3sKLgC2omdhRYrtthRopiODRgsNPNkuBMQPIuXavoig.lua.txt"))()
