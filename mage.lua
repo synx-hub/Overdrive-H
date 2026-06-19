439b709ab05f10c2d9eb4bc1500747628633f61e
synx-hub <azuresp3@gmail.com>
2026-06-19 18:11:50 +0800

    Update mage.lua

--- mage.lua
+++ mage.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/synx-hub/Overdrive-H/refs/heads/main/obfuscated_FP7TDl09i3F1Yx4tnq6Tc2cTmMEWiTqV4G8PN2g3Bm2NO5fUkNHmZp8ipRpffZ1enMiMz.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/synx-hub/dev/refs/heads/main/obfuscated_0JuHjzfo6xInyOLlYnRuotK9KW3sKLgC2omdhRYrtthRopiODRgsNPNkuBMQPIuXavoig.lua.txt"))()
