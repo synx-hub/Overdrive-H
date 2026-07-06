c91d3f653fa1cac081775da5e0225fd8cdefeac4
synx-hub <azuresp3@gmail.com>
2026-07-06 09:33:17 +0800

    Update Loader.Lua

--- Loader.Lua
+++ Loader.Lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/oRh7yzFCmU/0"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/synx-hub/Overdrive-H/refs/heads/main/obfuscated_4OeRRBkWX34L96OS2ox0vLonDtZXl3dHJmwa9ENJKatpZXXvzLQDGUOZ4ZnUpI5oLZZD5.lua.txt"))()
